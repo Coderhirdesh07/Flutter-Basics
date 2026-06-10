871d4ff78ff25322778ab61af049db80284b7dd8
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-06-11 00:13:45 +0530

    initial project setup

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        body: SizedBox(
+          height: 20,
+          width: 20,
+        ),
+      )
+    );
+  }
+}
