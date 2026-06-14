a4926ec29d64f3d3d0203c1315b5156ca7006075
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-06-14 12:08:51 +0530

    added a simple todo app code with ui and logic

--- lib/component/todo.dart
+++ lib/component/todo.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoList extends StatelessWidget {
-  const ToDoList({super.key});
+  final String taskName;
+  final bool taskCompleted;
+
+  Function(bool?)? onChanged;
+  ToDoList({super.key,
+  required this.taskName,required this.taskCompleted,required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(padding: const EdgeInsets.all(25.0),
-     child: Container(
+    return  Padding(
+      padding: const EdgeInsets.only(left: 25,top: 25,right:25),
+      child: Slidable( 
+        endActionPane: ActionPane(motion: StretchMotion(), children: [SlidableAction(onPressed: deleteFunction,icon:Icons.delete,backgroundColor: Colors.red.shade100,)]),
       padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(color: Colors.yellow,borderRadius: BorderRadius.circular(12)),
       child: Row(
         children: [
-          Checkbox(value: value, onChanged: onChanged),
-          Text("Make Tutorial"),
+          Checkbox(value: taskCompleted, 
+          onChanged: onChanged,
+          activeColor: Colors.black),
+
+          Text(taskName,style: TextStyle(decoration: taskCompleted?TextDecoration.lineThrough:TextDecoration.none),)
         ],
       ),
-      decoration: BoxDecoration(color: Colors.yellow,borderRadius: BorderRadius.circular(12)),
      ),
     );
   }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,8 +1,46 @@
 import "package:flutter/material.dart";
 import "../component/todo.dart";
+import '../component/dialog.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+  List todoList = [
+    ["Make Tutorial",false],
+    ["Do Excersice",false]
+  ];
+  void checkBoxChanged(bool? value,int index){
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+  void saveNewTask(){
+    setState(() {
+      todoList.add([_controller.text,false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  void createNewTask(){
+    showDialog(context: context,
+     builder: (context) {
+      return DialogBox(controller: _controller,onSave: saveNewTask,onCancel: ()=>Navigator.of(context).pop(),);
+     },
+    );
+  }
+
+  void deleteTask(int index){
+    setState(() {
+      todoList.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +50,18 @@ class HomePage extends StatelessWidget {
         title: Text("To Do"),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoList(),
-        ],
+      floatingActionButton: FloatingActionButton(onPressed: createNewTask),
+      body: ListView.builder(
+        itemCount:todoList.length ,
+        itemBuilder: (context,index){
+          return ToDoList(
+            taskName: todoList[index][0],
+            taskCompleted: todoList[index][1], 
+            onChanged: (value) => checkBoxChanged(value,index),
+            deleteFunction: (context)=> deleteTask(index),
+            );
+        },
       ),
-    );
+      );
   }
 }
